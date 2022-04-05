264ecbb7da3f557c2593f98ac3aae390a2367658
jakepurple13 <jakerein@gmail.com>
2022-04-05 08:52:13 -0400

    - super small cleanup

--- app/src/main/java/com/programmersbox/androidxreleasenotes/MainActivity.kt
+++ app/src/main/java/com/programmersbox/androidxreleasenotes/MainActivity.kt
@@ -2,24 +2,18 @@ package com.programmersbox.androidxreleasenotes
 
 import android.content.Intent
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.text.Html
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.ListItem
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -32,10 +26,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.fastForEach
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.work.Constraints
-import androidx.work.NetworkType
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.programmersbox.androidxreleasenotes.ui.theme.AndroidXReleaseNotesTheme
@@ -55,8 +45,6 @@ class MainActivity : ComponentActivity() {
 fun ReleaseNoteScreen(vm: ReleaseNotesViewModel = viewModel()) {
     val scrollBehavior = remember { TopAppBarDefaults.pinnedScrollBehavior() }
 
-    val context = LocalContext.current
-
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
@@ -64,6 +52,9 @@ fun ReleaseNoteScreen(vm: ReleaseNotesViewModel = viewModel()) {
                 title = { Text("AndroidX Release Notes") },
                 scrollBehavior = scrollBehavior,
                 /*actions = {
+
+                    val context = LocalContext.current
+
                     IconButton(
                         onClick = {
                             WorkManager.getInstance(context)
